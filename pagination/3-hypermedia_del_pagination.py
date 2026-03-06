704cebc5d6a57b93b8285f5b41ee8a9ebe0721d5
TariqRash <tariq@hostworksa.com>
2026-03-07 02:57:52 +0300

    Fix Task 3: update get_hyper_index implementation

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -39,30 +39,44 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,
-                        page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
-        Return a deletion-resilient hypermedia pagination dictionary.
+            Get the hyper index
 
-        Args:
-            index: the start index of the current page.
-            page_size: the number of items per page.
+            Args:
+                index: Current page
+                page_size: Total size of the page
 
-        Return:
-            A dict with index, next_index, page_size, data.
+            Return:
+                Hyper index
         """
-        indexed = self.indexed_dataset()
-        assert index is not None and index >= 0 and \
-            index < len(indexed)
-        data = []
-        current = index
-        while len(data) < page_size:
-            if current in indexed:
-                data.append(indexed[current])
-            current += 1
+        result_dataset = []
+        index_data = self.indexed_dataset()
+        keys_list = list(index_data.keys())
+        assert index + page_size < len(keys_list)
+        assert index < len(keys_list)
+
+        if index not in index_data:
+            start_index = keys_list[index]
+        else:
+            start_index = index
+
+        for i in range(start_index, start_index + page_size):
+            if i not in index_data:
+                result_dataset.append(index_data[keys_list[i]])
+            else:
+                result_dataset.append(index_data[i])
+
+        next_index: int = index + page_size
+
+        if index in keys_list:
+            next_index
+        else:
+            next_index = keys_list[next_index]
+
         return {
             'index': index,
-            'data': data,
-            'page_size': len(data),
-            'next_index': current
+            'next_index': next_index,
+            'page_size': len(result_dataset),
+            'data': result_dataset
         }
