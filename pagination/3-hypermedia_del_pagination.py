102d39ce36bbe1d03842d0a4e8347225ab75e7fe
TariqRash <tariq@hostworksa.com>
2026-03-07 03:00:43 +0300

    Task 3: Deletion-resilient hypermedia pagination

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -39,7 +39,8 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None,
+                        page_size: int = 10) -> Dict:
         """
             Get the hyper index
 
@@ -50,33 +51,20 @@ class Server:
             Return:
                 Hyper index
         """
-        result_dataset = []
         index_data = self.indexed_dataset()
         keys_list = list(index_data.keys())
-        assert index + page_size < len(keys_list)
-        assert index < len(keys_list)
-
-        if index not in index_data:
-            start_index = keys_list[index]
-        else:
-            start_index = index
-
-        for i in range(start_index, start_index + page_size):
-            if i not in index_data:
-                result_dataset.append(index_data[keys_list[i]])
-            else:
-                result_dataset.append(index_data[i])
-
-        next_index: int = index + page_size
-
-        if index in keys_list:
-            next_index
-        else:
-            next_index = keys_list[next_index]
-
+        assert index is not None
+        assert index >= 0 and index < len(keys_list)
+        data = []
+        current = index
+        while len(data) < page_size and current < max(
+                keys_list) + 1:
+            if current in index_data:
+                data.append(index_data[current])
+            current += 1
         return {
             'index': index,
-            'next_index': next_index,
-            'page_size': len(result_dataset),
-            'data': result_dataset
+            'data': data,
+            'page_size': len(data),
+            'next_index': current
         }
