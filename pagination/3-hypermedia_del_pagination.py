48385a223924ad48ab84e43e10bb472e8b537ba0
TariqRash <tariq@hostworksa.com>
2026-03-07 03:06:47 +0300

    Fix Task 3: index full dataset for deletion resilience

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -30,9 +30,8 @@ class Server:
         """
         if self.__indexed_dataset is None:
             dataset = self.dataset()
-            truncated_dataset = dataset[:1000]
             self.__indexed_dataset = {
-                i: truncated_dataset[i] for i in range(len(truncated_dataset))
+                i: dataset[i] for i in range(len(dataset))
             }
         return self.__indexed_dataset
 
