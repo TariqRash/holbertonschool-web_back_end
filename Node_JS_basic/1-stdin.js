5cbe7d317e2b9a6b8d002263199ededeca0020b1
TariqRash <tariq@hostworksa.com>
2026-04-11 10:57:20 +0300

    Fix stdin lifecycle handling for Node_JS_basic task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
+process.stdin.resume();
 
 process.stdin.on('data', (chunk) => {
   process.stdout.write(`Your name is: ${chunk.trim()}\n`);
